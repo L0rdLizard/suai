24b95bf77e3c9beeee688c397201aa04339dcc60
LordLizard Notebook <WarriorXKZ@gmail.com>
2021-10-23 11:49:58 +0300

    lab3middle

--- labs/lab3/lab03.c
+++ labs/lab3/lab03.c
@@ -2,7 +2,8 @@
 
 // Написать функцию, которая из двух массивов типа int, упорядоченных по убыванию, формирует новый массив двойной длины, упорядоченный по убыванию (слияние).
 
-int merge(int arr1[], int arr2[]){
+int merge(int* arr1, int* arr2){
+   int i=0, j=0;
 
 }
 
@@ -15,8 +16,19 @@ int main (int argc, char *argv[]) {
    //там чат есть нормальный
    //gde
    //слева live share и там чат в сеансе
-   int arr1[100], arr2[100], arr3[200];
-   fgets(arr1, 100, stdin);
-   fgets(arr2, 100, stdin);
+   int array1[101] = {0}, array2[101] = {0}, array3[202] = {0};
+   int count1 = 0, count2 = 0;
+   fgets(array1, 101, stdin);
+   fgets(array2, 101, stdin);
+   for (int i = 0; i <= 101; i++){
+      if (array1[i] != 0) count1++;
+   }
+   for (int i = 0; i <= 101; i++){
+      if (array2[i] != 0) count2++;
+   }
+   int merge(array1, array2);
+   for (int i = 0; i <= (count1+count2); i++)
+                    printf("%d", array3[i]);
+                printf("\n");
    return 0;
 }
