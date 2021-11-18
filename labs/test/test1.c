29bf388230ea3e6384df406b5b068ff84bbb5a9e
LordLizard <WarriorXKz@gmail.com>
2021-11-19 02:36:35 +0300

    test 1 p

--- labs/test/test1.c
+++ labs/test/test1.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+void hi(int **d){
+    d[0] = 2;
+    return 0;
+}
 int main(int argc, char const *argv[])
 {
-    for(int i=0; i<500; i++){
-        printf("%d %c\n", i, i);
-    }
+    int c[] = {5, 4, 3, 2};
+    hi(&c);
+    printf("%d %d %d %d", c[0],c[1],c[2],c[3]);
     return 0;
 }
