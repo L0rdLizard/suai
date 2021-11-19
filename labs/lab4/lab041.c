14e4ee42219e47f36b9825ec6944aed786201282
LordLizard <WarriorXKz@gmail.com>
2021-11-19 21:52:17 +0300

    lab4 done

--- labs/lab4/lab041.c
+++ labs/lab4/lab041.c
@@ -9,9 +9,19 @@ int is_char(int ch)
    else
       return 0;
 }
-
-// ab
-// abc
+// contain; return 1
+// not contain; return -1
+int contain(char *str, int w1s, int w1e, int *newstartmass, int *newendmass, int cur_word){
+   for (int i=0; i<cur_word; i++){
+      if (newstartmass[i] == -1) break;
+      if ((w1e-w1s)!=(newendmass[i]-newstartmass[i])) continue;
+      int flag = 1;
+      for (int j=0; j < (w1e-w1s+1); j++)
+         if (str[w1s+j] != str[newstartmass[i]+j]) flag = 0;
+      if (flag == 1) return 1;
+   }
+   return -1;
+}
 
 // a higher then b; return 1
 // b higher then a; return -1
@@ -34,19 +44,16 @@ int dif(char *str, int w1s, int w1e, int w2s, int w2e)
 void sorting(char *str, int *startmass, int *endmass, int cur_word)
 {
    int i = 0, j = 0;
-   for (i = 0; i < cur_word; i++)
+   for (int i = 0; i < cur_word - 1; i++)
    {
-      for (j = 0; j < cur_word; j++)
+      for (int j = (cur_word-1); j > i; j--)
       {
-         if (dif(str, startmass[j], endmass[j], startmass[j+1], endmass[j+1]) == -1)
+         if (dif(str, startmass[j], endmass[j], startmass[j-1], endmass[j-1]) == 1)
          {
-            printf("%d %d\n", startmass[j], startmass[j+1]);
-            printf("%d %d\n", endmass[j], endmass[j+1]);
-            printf("\n");
-            int tmpst = startmass[j+1];
-            int tmpend = endmass[j+1];
-            startmass[j+1] = startmass[j];
-            endmass[j+1] = endmass[j];
+            int tmpst = startmass[j-1];
+            int tmpend = endmass[j-1];
+            startmass[j-1] = startmass[j];
+            endmass[j-1] = endmass[j];
             startmass[j] = tmpst;
             endmass[j] = tmpend;
          }
@@ -58,11 +65,13 @@ int main(int argc, char *argv[])
 {
    char str[101] = {0};
    fgets(str, 101, stdin);
-   int startmass[100] = {0}, endmass[100] = {0}, end = -1, start = -1;
+   int startmass[100] = {0}, endmass[100] = {0}, newstartmass[100] = {0}, newendmass[100] = {0}, end = -1, start = -1;
    for (size_t i = 0; i < 100; i++)
    {
       startmass[i] = -1;
       endmass[i] = -1;
+      newstartmass[i] = -1;
+      newendmass[i] = -1;
    }
    int cur_word = 0;
    for (int i = 0; i < 101; i++)
@@ -95,10 +104,18 @@ int main(int argc, char *argv[])
          }
       }
    }
-   sorting(str, startmass, endmass, cur_word );
-   for (int i=0; i < cur_word; i++){
-      for (int j=0; j < (endmass[i]-startmass[i]+1); j++){
-         printf("%c",str[startmass[i]+j]);
+   sorting(str, startmass, endmass, cur_word);
+   int count = 0;
+   for (int i = 0; i < cur_word; i++){
+      if(contain(str, startmass[i], endmass[i], newstartmass, newendmass, cur_word) == -1){
+         newstartmass[count] = startmass[i];
+         newendmass[count] = endmass[i];
+         count++;
+      }
+   }
+   for (int i=0; i < count; i++){
+      for (int j=0; j < (newendmass[i]-newstartmass[i]+1); j++){
+         printf("%c",str[newstartmass[i]+j]);
       }
       printf("\n");
    }
