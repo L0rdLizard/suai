130bdc89938b4180cf020b8a786e14e3905cbf94
LordLizard <WarriorXKz@gmail.com>
2022-02-20 22:34:26 +0300

    homework1

---
+++ labs/osn/homework1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#define count 8
+int counter(int data)
+{
+    int x = 1, w = 0;
+    x = data;
+    while (x > 0)
+    {
+        w = w + (x & 1);
+        x = x >> 1;
+    }
+    printf("%d", w);
+
+    return w;
+}
+
+int main(int argc, char *argv[])
+{
+    int mass[count];
+    for (int i = 0; i < count; i++)
+    {
+        mass[i] = counter(i);
+    }
+    printf("\n");
+    int input;
+    while (1)
+    {
+        scanf_s("%d", &input);
+        if (input <= -1)
+            break;
+        else if (input >= count)
+            break;
+        printf("%d\n", mass[input]);
+    }
+    return 0;
+}
--- labs/osn/lesson.c
+++ labs/osn/lesson.c
@@ -3,13 +3,17 @@
 int main(int argc, char *argv[])
 {
     int x = 1, w = 0;
-    scanf_s("%d", &x);
-    while (x > 0)
+    do
     {
-        w = w + (x & 1);
-        x = x >> 1;
-    }
-    printf("%d", w);
-
+        scanf_s("%d", &x);
+        int z = x;
+        while (z > 0)
+        {
+            w = w + (z & 1);
+            z = z >> 1;
+        }
+        printf("%d\n", w);
+        w = 0;
+    } while (x != 0);
     return 0;
 }
