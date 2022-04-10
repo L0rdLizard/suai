1a939eebe7bd2d9b798c5efbbbdead4822165c8c
LordLizard <WarriorXKz@gmail.com>
2022-04-10 16:28:13 +0300

    8 dop start

--- labs/lab08dop/lab08dop.cpp
+++ labs/lab08dop/lab08dop.cpp
@@ -43,6 +43,47 @@ void create_polinom(node *&top, node *&end, int n)
     }
 }
 
+void addToPoly(node*& top, node*& end, int power, int koef, int counter_nnode)
+{
+    // cout << "counter node: " << counter_nnode;
+    node* p = new node;
+    p->power = power;
+    p->koef = koef;
+    if (counter_nnode == 0)
+    {
+        p->next = top;
+        top = p;
+        end = p;
+    }
+    else
+    {
+        end->next = p;
+        p->next = NULL;
+        end = p;
+    }
+}
+
+void del(node *top1, node *top2, node *top3, node *end3)
+{
+    int counter = 0;
+    while ((top1 != NULL) && (top1->power >= top2->power))
+    {
+        node *top_cur;
+        node *end_cur;
+        double temp_koef = top1->koef / top2->koef;
+        int temp_power = top1->power - top2->power;
+
+        addToPoly(top3, end3, (top1->power - top2->power), (top1->koef / top2->koef), counter);
+
+        node *cur_temp = top2;
+        int counter2 = 0;
+        while (cur_temp != NULL)
+        {
+            addToPoly(top_cur, end_cur, cur_temp->power + temp_power, cur_temp->koef * temp_koef, counter2);
+        }
+    }
+}
+
 double F2(node *&top, double x)
 {
     double result = 0;
@@ -68,14 +109,4 @@ double FindRoot(double (*f)(node*&, double), double a, double b, double eps, nod
             b = c;
     }
     return c;
-}
-
-void del(node *&top1, node *&top2, node *&res, int n1, int n2){
-    node *cur1 = top1;
-    node *cur2 = top2;
-    int i;
-    int n = max(n1, n2);
-    for (i = 0; i < n; i++){
-        
-    }
 }
--- labs/lab08dop/main08dop.cpp
+++ labs/lab08dop/main08dop.cpp
@@ -7,20 +7,26 @@ using namespace std;
 
 int main()
 {
-    node *top_polinom, *end_polinom;
-    int n;
-    cout << "enter n = ";
-    cin >> n;
+    node *top_polinom1, *end_polinom1;
+    node *top_polinom2, *end_polinom2;
+    node *top_polinom3, *end_polinom3;
+    int n1;
+    cout << "enter n1 = ";
+    cin >> n1;
     cout << "\n";
-    // node **mass = (node **)malloc(sizeof(node *) * n);
-    create_polinom(top_polinom, end_polinom, n);
+    create_polinom(top_polinom1, end_polinom1, n1);
+
+    int n2;
+    cout << "enter n2 = ";
+    cin >> n2;
+    cout << "\n";
+    create_polinom(top_polinom2, end_polinom2, n2);
 
     double a, b, eps, x;
     cout << "interval: ";
     cin >> a;
     cin >> b;
-    // cout << F2(top_polinom, a) * F2(top_polinom, b) << "\n";
-    if (F2(top_polinom, a) * F2(top_polinom, b) > 0)
+    if (F2(top_polinom1, a) * F2(top_polinom1, b) > 0)
     {
         cout << "Wrong data or interval!\n";
         return 0;
@@ -28,7 +34,7 @@ int main()
     cout << "eps: ";
     cin >> eps;
 
-    x = FindRoot(F2, a, b, eps, top_polinom);
+    x = FindRoot(F2, a, b, eps, top_polinom1);
     cout << "x = " << x << endl;
 
     return 0;
