4c446fb971123b1f3fd9380ca683bcdc912613a5
LordLizard <WarriorXKz@gmail.com>
2022-05-06 22:57:48 +0300

    lab09 fix malloc

--- labs/lab09/lab09.cpp
+++ labs/lab09/lab09.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 using namespace std;
 
-void summ(char *arr1, char *arr2, int *result)
+void summ(char *arr1, char *arr2, int *result, int n)
 {
-    for (int i = 5; i >= 0; i--)
+    for (int i = n-2; i >= 0; i--)
     {
         int temp_result = ((int)arr1[i] - 48) + ((int)arr2[i] - 48);
         result[i + 1] += temp_result;
@@ -15,21 +15,21 @@ void summ(char *arr1, char *arr2, int *result)
             result[i + 1] %= 10;
         }
     }
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < n-1; i++)
         cout << arr1[i];
     cout << " + ";
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < n-1; i++)
         cout << arr2[i];
     cout << " = ";
-    for (int i = result[0] == 0 ? 1 : 0; i < 7; i++)
+    for (int i = result[0] == 0 ? 1 : 0; i < n; i++)
         cout << result[i];
     cout << endl;
 }
 
-void razn(char *arr1, char *arr2, int *result)
+void razn(char *arr1, char *arr2, int *result, int n)
 {
     int flag = 0;
-    for (int i = 5; i >= 0; i--)
+    for (int i = n-1; i >= 0; i--)
     {
         int temp_result = ((int)arr1[i] - 48) - ((int)arr2[i] - 48) - flag;
         flag = 0;
@@ -41,13 +41,13 @@ void razn(char *arr1, char *arr2, int *result)
             result[i] = temp_result2;
         }
     }
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < n; i++)
         cout << arr1[i];
     cout << " - ";
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < n; i++)
         cout << arr2[i];
     cout << " = ";
-    for (int i = result[0] == 0 ? 1 : 0; i < 6; i++)
+    for (int i = result[0] == 0 ? 1 : 0; i < n; i++)
         cout << result[i];
     cout << endl;
 }
--- labs/lab09/lab09.h
+++ labs/lab09/lab09.h
@@ -1,8 +1,8 @@
 #ifndef LAB09_H_
 #define LAB09_H_
 
-void summ(char *arr1, char *arr2, int *result);
+void summ(char *arr1, char *arr2, int *result, int n);
 
-void razn(char *arr1, char *arr2, int *result);
+void razn(char *arr1, char *arr2, int *result, int n);
 
 #endif
