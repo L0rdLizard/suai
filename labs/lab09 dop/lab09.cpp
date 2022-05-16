2de8f17b81ede03a3942ecda2f59092327cb6a4f
LordLizard <WarriorXKz@gmail.com>
2022-05-16 13:50:39 +0300

    lab09 dop umn

--- labs/lab09 dop/lab09.cpp
+++ labs/lab09 dop/lab09.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void summ(char *arr1, char *arr2, int *result, int n)
 {
-    for (int i = n-2; i >= 0; i--)
+    for (int i = n - 2; i >= 0; i--)
     {
         int temp_result = ((int)arr1[i] - 48) + ((int)arr2[i] - 48);
         result[i + 1] += temp_result;
@@ -15,10 +15,10 @@ void summ(char *arr1, char *arr2, int *result, int n)
             result[i + 1] %= 10;
         }
     }
-    for (int i = 0; i < n-1; i++)
+    for (int i = 0; i < n - 1; i++)
         cout << arr1[i];
     cout << " + ";
-    for (int i = 0; i < n-1; i++)
+    for (int i = 0; i < n - 1; i++)
         cout << arr2[i];
     cout << " = ";
     for (int i = result[0] == 0 ? 1 : 0; i < n; i++)
@@ -29,7 +29,7 @@ void summ(char *arr1, char *arr2, int *result, int n)
 void razn(char *arr1, char *arr2, int *result, int n)
 {
     int flag = 0;
-    for (int i = n-1; i >= 0; i--)
+    for (int i = n - 1; i >= 0; i--)
     {
         int temp_result = ((int)arr1[i] - 48) - ((int)arr2[i] - 48) - flag;
         flag = 0;
@@ -45,6 +45,35 @@ void razn(char *arr1, char *arr2, int *result, int n)
         cout << arr1[i];
     cout << " - ";
     for (int i = 0; i < n; i++)
+        cout << arr2[i];
+    cout << " = ";
+    for (int i = result[0] == 0 ? 1 : 0; i < n; i++)
+        cout << result[i];
+    cout << endl;
+}
+
+void umn(char *arr1, char *arr2, int *result, int size_a, int size_b)
+{
+    int n = size_a + size_b + 1;
+    for (int i = 0; i < size_a; i++)
+        for (int j = 0; j < size_b; j++)
+        {
+            result[i + j - 1] += ((int)arr1[i] - 48) * ((int)arr2[j] - 48);
+            cout << result[i + j - 1];
+        }
+    for (int i = 0; i < n; i++)
+    {
+        result[i + 1] += result[i] / 10;
+        result[i] %= 10;
+    }
+
+    while (result[n] == 0)
+        n--;
+
+    for (int i = 0; i < size_a; i++)
+        cout << arr1[i];
+    cout << " * ";
+    for (int i = 0; i < size_b; i++)
         cout << arr2[i];
     cout << " = ";
     for (int i = result[0] == 0 ? 1 : 0; i < n; i++)
--- labs/lab09 dop/lab09.h
+++ labs/lab09 dop/lab09.h
@@ -5,4 +5,6 @@ void summ(char *arr1, char *arr2, int *result, int n);
 
 void razn(char *arr1, char *arr2, int *result, int n);
 
+void umn(char *arr1, char *arr2, int *result, int size_a, int size_b);
+
 #endif
