6b15283a05e00c6770ab86264af045f07f0495b8
LordLizard <WarriorXKz@gmail.com>
2022-05-30 00:01:37 +0300

    pr2

---
+++ labs/practika2/pr1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+using namespace std;
+static unsigned char A[] = {0x00, 0x0d, 0x16, 0x1b};
+
+unsigned char coder(unsigned char m) { return A[m]; }
+unsigned char channel(unsigned char a, unsigned char e) { return a ^ e; }
+unsigned char count_ones(unsigned char n)
+{
+    unsigned char w = 0;
+    while (n > 0)
+    {
+        w += n & 1;
+        n = n >> 1;
+    }
+    return w;
+}
+unsigned char hemming(unsigned char a, unsigned char b)
+{
+    return count_ones(a ^ b);
+}
+unsigned char decoder(unsigned char m)
+{
+    unsigned char ind = 0, min = 5;
+    for (unsigned char i = 0; i < 4; i++)
+    {
+        unsigned char d = hemming(m, A[i]);
+        if (d < min)
+        {
+            min = d;
+            ind = i;
+        }
+    }
+    return ind;
+}
+string bin(unsigned char n)
+{
+    string bin_code;
+    for (unsigned char i = 0; i < 5; i++)
+        bin_code += to_string((n >> (4 - i)) & 1);
+    return bin_code;
+}
+int main()
+{
+    unsigned long long m, e, w, c, d;
+    cin >> m >> e;
+    w = coder(m);
+    c = channel(w, e);
+    d = decoder(c);
+    cout << "m:	" << bin(m) << endl;
+    cout << "w:	" << bin(w) << endl;
+    cout << "e:	" << bin(e) << endl;
+    cout << "c:	" << bin(c) << endl;
+    cout << "d:	" << bin(d) << endl;
+}
---
+++ labs/practika2/pr3.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <math.h>
+#define N 5
+#define D 3
+typedef unsigned long long uint64;
+using namespace std;
+uint64 count_ones(uint64 n)
+{
+    uint64 w = 0;
+    while (n > 0)
+    {
+        w += n & 1;
+        n = n >> 1;
+    }
+    return w;
+}
+// Исходными данными программы является N - максимальная длина кода, D - минимальное расстояние кода.
+int main()
+{
+    size_t M = 0; // заводим счётчик числа кодовых слов в книге
+    uint64 size = pow(2, N);
+    uint64 mark[32] = {0};     // создаём вспомогательный массив для пометок заполненный 0
+    uint64 CodeBook[32] = {0}; // Заводим массив для списка кодовых слов с запасом.
+    uint64 zero = 0;
+
+    while (1) // выолняем шаги в цикле до тех пор пока нули находятся в пределах длины:
+    {
+        //
+        for (zero = 0; (mark[zero] != 0 && zero < size); zero++)
+            ; // Шаг 0 выполняем поиск первой свободной последовательности в массиве mark, записываем в zero
+        if (zero >= size)
+            break;          // Если Шаг 0 перебрал все элементы выходим из
+        CodeBook[M] = zero; // Шаг 1 Выберем в качестве кодового слова первую свободную последовательнотсть
+        mark[zero] = 2;
+        M++;
+
+        for (uint64 i = 0; i < size; i++)
+        {
+            if (mark[i] == 0 && (count_ones(i ^ CodeBook[M - 1]) < D))
+                mark[i] = 1;
+            cout << mark[i] << " ";
+        }
+        cout << endl;
+    }
+    cout << "Answers: ";
+    for (size_t i = 0; i < M; i++)
+        cout << CodeBook[i] << " ";
+    cout << endl
+         << M << endl;
+}
