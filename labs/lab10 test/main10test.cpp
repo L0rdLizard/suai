86db1c7f48320e323ac1b2299c00671f94845b00
LordLizard <WarriorXKz@gmail.com>
2022-07-04 01:04:58 +0300

    lab10 found problem

--- labs/lab10 test/main10test.cpp
+++ labs/lab10 test/main10test.cpp
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <string.h>
- 
+#include <cstring>
+
 int Search(char line[], char word[])
 {
     char *p, *start;
     int count = 0;
     start = line;
-        while(1){
-            p = strstr(start, word);
-            if(p == NULL)break;
-            count++;
-            start = p + strlen(word);
-        }
+    while (1)
+    {
+        p = strstr(start, word);
+        if (p == NULL)
+            break;
+        count++;
+        start = p + strlen(word);
+    }
     return count;
 }
 int main()
@@ -20,15 +23,25 @@ int main()
     char s[128];
     FILE *fp;
     fp = fopen("f1.txt", "r");
-    char* a = "for";
-    while(fgets(s, 128, fp)){
+    char *a = "for";
+    // while (fgets(s, 128, fp) != NULL)
+    // {
+    //     nf += Search(s, a);
+    //     nw += Search(s, "while");
+    //     nd += Search(s, "do");
+    //     ni += Search(s, "if");
+    // }
+    // while (!feof(fp))
+    // {
+    //     if (fgets(s, 128, fp))
+    //         nf += Search(s, a);
+    // }
+
+    // fclose(fp);
+    if (fgets(s, 128, fp)){
         nf += Search(s, a);
-        // nw += Search(s, "while");
-        // nd += Search(s, "do");
-        // ni += Search(s, "if");
     }
-    fclose(fp);
     puts("File f1 has words:");
-    printf("'for' %d times,\n'while' %d times,\n'do' %d times,\n and 'if' %d times.\n", nf, nw, nd, ni);
+    printf("'for' %d times,\n'while' %d times,\n'do' %d times,\n'if' %d times.\n", nf, nw, nd, ni);
     return 0;
 }
