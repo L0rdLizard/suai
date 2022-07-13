1295f4c1619aad2ff68f15db50505ef8cb24b231
LordLizard <WarriorXKz@gmail.com>
2022-07-13 17:37:25 +0300

    test strlen

---
+++ labs/test/teststring.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+    char str[256] = "Hello";
+    cout << str << endl;
+    int a = 0, b = 0;
+    while(1) {
+        int count = 0;
+        if (str[b] == NULL) break;
+        b++; 
+    }
+    a = strlen(str);
+    cout << a << endl;
+    cout << b << endl;
+
+    
+    return 0;
+}
