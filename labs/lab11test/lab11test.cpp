923652c9ad63a73f99aee802e566bc4101bc0483
LordLizard <WarriorXKz@gmail.com>
2022-07-16 21:27:55 +0300

    lab11 test

---
+++ labs/lab11test/lab11test.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+  char str[] = "Hello, my name is Max";
+  printf("Split \"%s\" on lexem.", str);
+  char *pch = strtok(str, " ,."); //во втором параметре указаны разделители (пробел, запятая, точка)
+  printf("\nLex:");
+  while (pch != NULL) //пока есть лексемы
+  {
+      printf("\n%s", pch);
+      pch = strtok(NULL, " ,.");
+  }
+  return 0;
+}
