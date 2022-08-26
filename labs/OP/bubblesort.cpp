3aa50e02be6365bb5fa8c901300d55bdaaad99eb
LordLizard <WarriorXKz@gmail.com>
2022-08-26 19:33:42 +0300

    bubble sort test

---
+++ labs/OP/bubblesort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ostream>
+using namespace std;
+
+void bubble(void* array, int n, int elem_size, int(*comparator)(const void*)){
+
+}
+
+int main()
+{
+    // int p[2];
+    // int *p = (int*)malloc(sizeof(int)*2);
+    // *(p+2) = 1;
+    // p[2] = 1;
+    return 0;
+}
