410c214bd9510b843076ad2f495de7bf473fdac7
LordLizard <WarriorXKz@gmail.com>
2022-08-26 19:34:47 +0300

    drob

--- labs/OP/Drob.cpp
+++ labs/OP/Drob.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
+#include <ostream>
 using namespace std;
 
-int main(){
-    class drob
-    {
-    private:
-        int p_a;
-        int p_b;
-    public:
-        drob (int a, int b) {
-            p_a = a;
-            p_b = b;
-        };
-        friend ostream& operator<<(ostream& os, const drob& d);
-        // drob& operator<<(ostream& os, const drob& d);
-    };
+class drob
+{
+private:
+    int p_a;
+    int p_b;
 
-    // drob& drob::operator<<(ostream& os, const drob& d)
-    // {
-    //     os<<d.p_a<<"/"<<d.p_b;
-    //     return os;
-    // }
+public:
+    drob(int a, int b)
+    {
+        p_a = a;
+        p_b = b;
+    };
+    friend ostream &operator<<(ostream &os, const drob &d);
+    // drob& operator<<(ostream& os, const drob& d);
+};
+
+ostream &operator<<(ostream &os, const drob &d)
+{
+    os << d.p_a << "/" << d.p_b;
+    return os;
+}
+
+int main()
+{
+
     return 0;
 }
