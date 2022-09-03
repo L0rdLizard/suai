e33cd163050d28466d6739029d69875a1edd3a1d
LordLizard <WarriorXKz@gmail.com>
2022-09-04 02:02:52 +0300

    Nastya 3

--- labs/Nastya/Pipito.cpp
+++ labs/Nastya/Pipito.cpp
@@ -3,11 +3,25 @@ using namespace std;
 
 int main()
 {
+    int mass[] = {15, 45, 55, 123, 3, 6, 33};
+    // // mass[0] = 5;
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     printf("%d\n", mass[i]);
+    // }
+
+    // if (mass[0] < mass[1])
+    // {
+    //     printf("pito\n");
+    // }
+
     int a = 0;
-    for (int i = 0; i < 10; i++)
+
+    while (mass[a] != 33)
     {
-        a+=i;
-        printf("%d\n", a);
+        printf("%d = %d ne podhodit\n", a, mass[a]);
+        a++;
     }
+    printf("%d\n", a);
     return 0;
 }
