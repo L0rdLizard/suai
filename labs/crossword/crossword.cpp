4fe22e9c484c60d336e769fb096bdab1a0166c34
LordLizard <WarriorXKz@gmail.com>
2022-09-11 15:07:36 +0300

    crossword

---
+++ labs/crossword/crossword.cpp
@@ -0,0 +1,38 @@
+// #include "stdafx.h"
+#include <iostream>
+#include <clocale>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+ 
+#include "crossword.h"
+ 
+using namespace std;
+ 
+string sharedLetters(string s1,string s2)
+ 
+{
+    string sharedletters = "";
+ 
+    string commonletter = "";
+ 
+    for(int i=0;i<s1.length();i++)
+    {
+        for (int j = 0; j < s2.length(); j++)
+        {
+ 
+            if (tolower(s1.at(i)) == tolower(s2.at(j)))
+            {
+                commonletter = s1.at(i);
+ 
+ 
+                sharedletters.append(commonletter);
+            }
+        }
+ 
+    }
+ 
+ 
+    return sharedletters;
+}
---
+++ labs/crossword/crossword.h
@@ -0,0 +1,12 @@
+#pragma once
+ 
+// #include "stdafx.h"
+#include <iostream>
+#include <clocale>
+#include <fstream>
+#include <string>
+ 
+using namespace std;
+//Описание функции в которой мы будем использовать CrosswordGenerator.h
+ 
+string sharedLetters(string s1, string s2);
