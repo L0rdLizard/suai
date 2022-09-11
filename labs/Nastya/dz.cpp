7f4b9033d7c6220ad86365e7a1ed9189f32e9ef8
LordLizard <WarriorXKz@gmail.com>
2022-09-11 15:08:00 +0300

    dz

---
+++ labs/Nastya/dz.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 1000;
+    while (a != -1)
+    {
+        printf("%d\n", a);
+        a = a - 7;
+    }
+    // printf("%d\n", a );
+    return 0;
+}
