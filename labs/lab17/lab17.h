f3c1ae17bf2fcb3c2c0651af2ce29f6a269b8e6d
LordLizard <WarriorXKz@gmail.com>
2022-10-06 01:02:37 +0300

    dop 17 yaml

--- labs/lab17/lab17.h
+++ labs/lab17/lab17.h
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <ostream>
+#include <fstream>
+#include "yaml-cpp/yaml.h"
 
 struct point
 {
@@ -14,26 +16,36 @@ struct point
     }
 };
 
+void operator << (YAML::Emitter& out, const point& p)
+{
+    out << YAML::BeginMap;
+    out << YAML::Key << "x" << YAML::Value << p.x;
+    out << YAML::Key << "y" << YAML::Value << p.y;
+    out << YAML::Key << "z" << YAML::Value << p.z;
+    out << YAML::EndMap;
+}
+
 
 template <typename T>
-class vector
+class vector_t
 {
     public:
-        vector(size_t size, const T& value);
-        ~vector();
+        vector_t(size_t size, const T& value);
+        ~vector_t();
         void push_back(const T& value);
         void erase(size_t i);
         T& at(size_t i);
         size_t size();
         T& operator[](size_t i);
         void insert(size_t i, const T& value);
+        void saveyaml(const std::string& filename);
     private:
         T* data;
         size_t size_p;
 };
 
 template <typename T>
-vector<T>::vector(size_t size, const T& value)
+vector_t<T>::vector_t(size_t size, const T& value)
 {
     size_p = size;
     data = new T[size];
@@ -44,13 +56,13 @@ vector<T>::vector(size_t size, const T& value)
 }
 
 template <typename T>
-vector<T>::~vector()
+vector_t<T>::~vector_t()
 {
     delete[] data;
 }
 
 template <typename T>
-void vector<T>::push_back(const T& value)
+void vector_t<T>::push_back(const T& value)
 {
     T* temp = new T[size_p + 1];
     for (size_t i = 0; i < size_p; i++)
@@ -64,7 +76,7 @@ void vector<T>::push_back(const T& value)
 }
 
 template <typename T>
-void vector<T>::erase(size_t i)
+void vector_t<T>::erase(size_t i)
 {
     if (i >= size_p)
     {
@@ -85,7 +97,7 @@ void vector<T>::erase(size_t i)
 }
 
 template <typename T>
-T& vector<T>::at(size_t i)
+T& vector_t<T>::at(size_t i)
 {
     if (i >= size_p)
     {
@@ -95,19 +107,19 @@ T& vector<T>::at(size_t i)
 }
 
 template <typename T>
-size_t vector<T>::size()
+size_t vector_t<T>::size()
 {
     return size_p;
 }
 
 template <typename T>
-T& vector<T>::operator[](size_t i)
+T& vector_t<T>::operator[](size_t i)
 {
     return data[i];
 }
 
 template <typename T>
-void vector<T>::insert(size_t i, const T& value)
+void vector_t<T>::insert(size_t i, const T& value)
 {
     if (i >= size_p)
     {
@@ -127,6 +139,22 @@ void vector<T>::insert(size_t i, const T& value)
     data = temp;
     size_p++;
 }
+
+template <typename T>
+void vector_t<T>::saveyaml(const std::string& filename)
+{
+    YAML::Emitter out;
+    out << YAML::BeginSeq;
+    for (size_t i = 0; i < size_p; i++)
+    {
+        out << data[i];
+    }
+    out << YAML::EndSeq;
+    std::ofstream fout(filename);
+    fout << out.c_str();
+}
+
+// template <typename T>
 
 
 
--- labs/lab17/main17.cpp
+++ labs/lab17/main17.cpp
@@ -4,20 +4,23 @@
 using namespace std;
 
 int main(){
-    vector<int> v(5, 1);
-    v.push_back(2);
+    // vector_t<int> v(5, 2);
+    vector_t<point> v(5, {1, 2, 3});
+    v.saveyaml("test.yaml");
 
-    for (size_t i = 0; i < v.size(); i++)
-    {
-        cout << v[i] << endl;
-    }
+    // v.push_back(2);
 
-    vector<point> v2(5, {1, 2, 3});
-    v2.push_back({4, 5, 6});
-    v2.insert(8, {7, 8, 9});
+    // for (size_t i = 0; i < v.size(); i++)
+    // {
+    //     cout << v[i] << endl;
+    // }
 
-    for (size_t i = 0; i < v2.size(); i++)
-    {
-        cout << v2[i] << endl;
-    }
+    // vector_t<point> v2(5, {1, 2, 3});
+    // v2.push_back({4, 5, 6});
+    // v2.insert(8, {7, 8, 9});
+
+    // for (size_t i = 0; i < v2.size(); i++)
+    // {
+    //     cout << v2[i] << endl;
+    // }
 }
