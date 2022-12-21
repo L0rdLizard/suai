befe6c8e85037c55900751caab584293e342c515
LordLizard <WarriorXKz@gmail.com>
2022-12-21 06:45:50 +0300

    coursework complete

--- labs/criscross/board.cpp
+++ labs/criscross/board.cpp
@@ -31,7 +31,7 @@ Board::Board(unsigned int a, unsigned int b)
 			locations[row][col] = 0;
 		}
 	}
-	place = 1;
+	place = 0;
 }
 
 Board::~Board()
@@ -59,51 +59,81 @@ char Board::getSpot(int i, int j)
 
 void Board::placeList(vector<string> &list)
 {
-	for (int i = 0; i < list.size(); i++)
+	int previous = 0;
+	while (1)
 	{
-		if (i == 0)
+		vector<int> temp;
+		for (int i = 0; i < list.size(); i++)
 		{
-			for (int j = 0; j < list[0].size(); j++)
+			bool add = true;
+			if (place == 0)
 			{
-				setSpot((list[0])[j], a / 2, (b - list[0].size()) / 2 + j);
-			}
-			locations[0][0] = a / 2;
-			locations[0][1] = (b - list[0].size()) / 2;
-		}
-		if (i != 0 && i % 2 == 0)
-		{
-			if (placeNextVer(list[i]))
-			{
-			}
-			else if (placeNextHor(list[i]))
-			{
-			}
-			else
-			{
-				cout << "Could not place word: " << list[i] << endl;
-				list[i] = " ";
+				for (int j = 0; j < list[0].size(); j++)
+				{
+					setSpot((list[0])[j], a / 2, (b - list[0].size()) / 2 + j);
+				}
+				locations[0][0] = a / 2;
+				locations[0][1] = (b - list[0].size()) / 2;
 				place++;
+				temp.push_back(i);
+				continue;
 			}
+			if (place != 0 && place % 2 == 0)
+			{
+				if (placeNextVer(list[i]))
+				{
+				}
+				else if (placeNextHor(list[i]))
+				{
+				}
+				else
+				{
+					add = false;
+					// cout << "Could not place word: " << list[i] << endl;
+					// list[i] = " ";
+					// place++;
+				}
+			}
+			else if (place != 0 && place % 2 != 0)
+			{
+				if (placeNextHor(list[i]))
+				{
+				}
+				else if (placeNextVer(list[i]))
+				{
+				}
+				else
+				{
+					add = false;
+					// cout << "Could not place word: " << list[i] << endl;
+					// list[i] = " ";
+					// place++;
+				}
+			}
+			if (add)
+				temp.push_back(i);
 		}
-		else if (i != 0 && i % 2 != 0)
+		if (temp.size() == previous)
+			break;
+		previous = temp.size();
+
+		for (int i = 0; i < temp.size(); i++)
 		{
-			if (placeNextHor(list[i]))
-			{
-			}
-			else if (placeNextVer(list[i]))
-			{
-			}
-			else
-			{
-				cout << "Could not place word: " << list[i] << endl;
-				list[i] = " ";
-				place++;
-			}
+			list.erase(list.begin() + temp[i] - i);
 		}
+	}
+	if (list.size() != 0)
+	{
+		cout << "Could not place words: ";
+		for (int i = 0; i < list.size(); i++)
+		{
+			cout << list[i] << " ";
+		}
+		cout << endl;
 	}
 }
 
-// placeNextVer method tries to find and place next word on board, vertically
+
 bool Board::placeNextVer(string word)
 {
 	int match = 0;
@@ -138,17 +168,17 @@ bool Board::placeNextVer(string word)
 					if (k == 0)
 						match = match + 2;
 				}
-			} // end of "k" for loop
+			}
 			if (match == 3 * word.size() + 2 && key == 1 && isPlaced == 0)
 			{
-				// place word
+
 				for (int p = 0; p < word.size(); p++)
 				{
 					setSpot(word[p], i + p, j);
 				}
-				// set isPlaced to true
+
 				isPlaced = 1;
-				// assign coordinates to the newly-placed word
+
 				locations[place][0] = i;
 				locations[place][1] = j;
 				place++;
@@ -156,12 +186,12 @@ bool Board::placeNextVer(string word)
 			}
 			match = 0;
 			key = 0;
-		}	  // end of second for loop (i)
-	}		  // end of outermost for loop (j)
-	return 0; // return false if the word was not able to be placed vertically
-} // end of placeNextVer method
+		}
+	}  
+	return 0; 
+} 
 
-// placeNextHor method tries to find and place next word on board, horizontally
+
 bool Board::placeNextHor(string word)
 {
 	int match = 0;
@@ -197,10 +227,10 @@ bool Board::placeNextHor(string word)
 						match = match + 2;
 				}
 
-			} // end of "k" for loop
+			} 
 			if (match == 3 * word.size() + 2 && key == 1 && isPlaced == 0)
 			{
-				// place word
+				
 				for (int p = 0; p < word.size(); p++)
 				{
 					setSpot(word[p], j, i + p);
@@ -209,29 +239,32 @@ bool Board::placeNextHor(string word)
 				locations[place][1] = i;
 				place++;
 
-				// set isPlaced to true
+
 				isPlaced = 1;
 				return 1;
 			}
 			match = 0;
 			key = 0;
-		}	  // end of second for loop (i)
-	}		  // end of outermost for loop (j)
-	return 0; // return false if the word was not able to be placed horizontally
-} // end of placeNextVer method
+		}
+	}
+	return 0; 
+}
 
-void Board::printBoard(){
-	for(int i=0; i<a; i++){
-		for(int j=0; j<b; j++){
+void Board::printBoard()
+{
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < b; j++)
+		{
 			cout << data[i][j];
-			
+
 			cout << " ";
 		}
 		cout << endl;
 	}
 }
 
-// output in file board.txt
+
 void Board::outputBoard(string output)
 {
 	ofstream fout;
