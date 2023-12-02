25856fac9502bf5dc267fd134142d2ff744bdb38
L0rdLizard <WarriorXKZ@gmail.com>
2023-12-02 20:26:02 +0300

    need to fix task

--- labs/lab4/lab04.c
+++ labs/lab4/lab04.c
@@ -10,7 +10,7 @@ void sort_words(char *words[], int count)
     {
         for (int j = i + 1; j < count; j++)
         {
-            if ((char)(*words[i]) < (char)(*words[j]))
+            if ((char)(*words[i]) > (char)(*words[j]))
             {
                 x = words[j];
                 words[j] = words[i];
@@ -23,12 +23,16 @@ void sort_words(char *words[], int count)
 int main()
 {
     char *words[] = { "cherry", "orange", "apple" };    
-    int count = 0;
+    int count = 3;
     //fgets(words, 101, stdin);
     sort_words(words, count);
-    printf("%c", words);
+    // printf("%c", words);
+    printf("%s", words[0]);
+    printf("%s", words[1]);
+    printf("%s", words[2]);
+    // printf("apple cherry orange");
     return 0;
 }
 
-Ребята я грустный туалет 
+// Ребята я грустный туалет 
 
